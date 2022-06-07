bef3345361094fed30988dab7215d13d41bd4c31
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2022-06-07 13:41:04 -0300

    now numbers are random
    using randint() and zfill() so the results now are random and fills 8 digits , getting closer to "real" numbers.

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
-x = 10005000
-y = 10000000
+x = 5001
+y = 1
 name = "person"
 
 while (y < x):
-    phone = str(y).zfill(8)
-    print("register( '" + str(name) + str(y)+ "', " + str(phone) + ").")
+    number = str(randint(00000000, 90000000)).zfill(8)
+    print("register('" + str(name) + str(y)+ "', " + str(number) + ").")
     y += 1
