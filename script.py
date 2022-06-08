366c243efc4edae1a614f0014a4aa109552974dd
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2022-06-07 22:03:07 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,14 @@
-x = 5001
-y = 1
-name = "person"
+from random import randint
+import sys
 
+#Treating parameters
+x = int(sys.argv[1])
+y = 0
+name = sys.argv[2]
+
+#executing looping while in range
 while (y < x):
+    #using zfill() function to fill the 8 digits
     number = str(randint(0, 90000000)).zfill(8)
-    print("register('" + str(name) + str(y)+ "', " + str(number) + ").")
+    print("register('" + str(name) + str(y)+ "', '" + str(number) + "').")
     y += 1
